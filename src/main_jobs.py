aa18d3701f535ec6c14dacd5afc1bba9eaf19df4
sdhslu <stefan.dreyfus@stud.hslu.ch>
2025-10-26 18:33:09 +0100

    switching order of analysis scripts executed by main script

--- src/main_jobs.py
+++ src/main_jobs.py
@@ -129,11 +129,35 @@ def run_full_data_pipeline(search_term: str, max_jobs: int, delete_session: bool
         sys.exit(1)
 
 
+    # --- SKILLS AND LOCATION ANALYSIS ---
+
+    if os.path.exists(SKILLS_INPUT_PATH):
+        try:
+            print("\n[5/6] Running Skills and Location Analysis")
+
+            analysis_success = analysis.analyze_jobs_texts_skills.run_skills_analysis(
+                input_file_path=SKILLS_INPUT_PATH,
+                output_dir_path=ANALYSIS_DATA_DIR
+            )
+
+            if analysis_success:
+                print("Skills and Location Analysis completed successfully.")
+            else:
+                print("Skills and Location Analysis failed. Check script logs.")
+                sys.exit(1)
+
+        except Exception as e:
+            print(f"SKILLS ANALYSIS CRITICAL FAILED: {e}");
+            sys.exit(1)
+    else:
+        print("\n[5/6] Skills Analysis step skipped: Clustered data file does not exist. Exiting.")
+        sys.exit(1)
+
     # --- SEMANTIC CLUSTERING ANALYSIS ---
     # Only run if the FINAL_CLEANED_PATH exists
     if os.path.exists(FINAL_CLEANED_PATH):
         try:
-            print("\n[5/6] Running Semantic Clustering Analysis")
+            print("\n[6/6] Running Semantic Clustering Analysis")
 
             analysis_success = analysis.analyze_jobs_semantic_clustering.run_semantic_clustering(
                 input_file_path=FINAL_CLEANED_PATH,
@@ -151,31 +175,7 @@ def run_full_data_pipeline(search_term: str, max_jobs: int, delete_session: bool
             print(f"CLUSTERING CRITICAL FAILED: {e}");
             sys.exit(1)
     else:
-        print("\n[5/6] Clustering step skipped: Final cleaned data file does not exist. Exiting.")
-        sys.exit(1)
-
-    # --- SKILLS AND LOCATION ANALYSIS ---
-
-    if os.path.exists(SKILLS_INPUT_PATH):
-        try:
-            print("\n[6/6] Running Skills and Location Analysis")
-
-            analysis_success = analysis.analyze_jobs_texts_skills.run_skills_analysis(
-                input_file_path=SKILLS_INPUT_PATH,
-                output_dir_path=ANALYSIS_DATA_DIR
-            )
-
-            if analysis_success:
-                print("Skills and Location Analysis completed successfully.")
-            else:
-                print("Skills and Location Analysis failed. Check script logs.")
-                sys.exit(1)
-
-        except Exception as e:
-            print(f"SKILLS ANALYSIS CRITICAL FAILED: {e}");
-            sys.exit(1)
-    else:
-        print("\n[6/6] Skills Analysis step skipped: Clustered data file does not exist. Exiting.")
+        print("\n[6/6] Clustering step skipped: Final cleaned data file does not exist. Exiting.")
         sys.exit(1)
 
 # --- FINAL STATUS ---
