efba4a86ea8f3e59c51fe39020584c2a43e53446
studerju <studerju@gmail.com>
2025-11-05 23:28:27 +0100

    map script BE corrected to 24, scripts and csvs not needed deleted

--- src/visualization/jobs_map.py
+++ src/visualization/jobs_map.py
@@ -70,7 +70,7 @@ def create_canton_map_visualization(job_counts_input_path: Path, report_output_p
         "CH - Zürich": "ZH", "Zentralschweiz": "LU", "Egerkingen": "SO", "Sierre": "VS",
         "Härkingen": "SO", "Thun, Berne, Schweiz": "BE", "Goldau / Schwyz / Uetendorf / Thun": "SZ",
         "2606 Corgémont": "BE", "Graubünden, Grono or remote": "GR", "Tägerwilen": "TG",
-        "Liebefeld": "BE", "Büren an der Aare": " BE", "Berna": "BE", "Zollikofen": "BE",
+        "Liebefeld": "BE", "Büren an der Aare": "BE", "Berna": "BE", "Zollikofen": "BE",
         "Lausanne, Switzerland": "VD", "Chiasso": "TI", "Landquart": "GR", "Baden": "AG",
         "Regensdorf": "ZH", "Dübendorf": "ZH", "Raum Bern und teilweise im Homeoffice": "BE",
         "Boudevilliers": "NE", "Pully": "VD", "Stabio": "TI", "Switzerland &gt; Basel : H-127 A2": "BS",
@@ -91,17 +91,27 @@ def create_canton_map_visualization(job_counts_input_path: Path, report_output_p
             print(missing["location"].unique())
 
         #  Aggregate job counts per canton
-        df_per_canton = (
-            df_job_count.groupby("canton", as_index=False)["job_count"]
-            .sum()
-            .sort_values("job_count", ascending=False)
-        )
+         #df_per_canton = (
+          #  df_job_count.groupby("canton", as_index=False)["job_count"]
+           # .sum()
+            #.sort_values("job_count", ascending=False)
+        #)
+
+        df_sum = df_job_count.groupby("canton", as_index=False)["job_count"].sum()
+        df_per_canton = df_sum.sort_values("job_count", ascending=False)
+
+        print("=== df_sum  ===")
+        print(df_sum)
+
+        print("\n=== df_per_canton  ===")
+        print(df_per_canton)
 
         df_per_canton['canton'] = df_per_canton['canton'].str.strip().str.upper()
         merged = gdf.merge(df_per_canton, left_on='id', right_on='canton', how='left')
 
         merged['job_count'] = merged['job_count'].fillna(0).astype(int)
         print(f"Merged cantons: {merged['job_count'].gt(0).sum()} with data out of {len(merged)} total.")
+
 
         # Save to new csv
         # Ensure the directory exists before saving
@@ -122,6 +132,8 @@ def create_canton_map_visualization(job_counts_input_path: Path, report_output_p
         # Replace NaN (no data) with 0
         merged['job_count'] = merged['job_count'].fillna(0).astype(int)
 
+
+
         # --- 4. Define classification bins ---
         bins = [-1, 0, 2, 4, 9, 15, 29, 59, float('inf')]
         labels = ['0', '1–2', '3–4', '5–9', '10–15', '16–29', '30–59', '60+']
@@ -139,6 +151,9 @@ def create_canton_map_visualization(job_counts_input_path: Path, report_output_p
         merged['job_class'] = pd.cut(merged['job_count'], bins=bins, labels=labels, include_lowest=True)
         color_map = dict(zip(labels, colors))
         merged['color'] = merged['job_class'].map(color_map).astype('object')
+
+        # check BE
+        print(merged.loc[merged['id'] == 'BE', ['id', 'job_count', 'job_class']])
 
         # --- 5. Plot ---
         # --- SETTINGS ---
@@ -171,10 +186,10 @@ def create_canton_map_visualization(job_counts_input_path: Path, report_output_p
             "BS": (3000, 6000),
             "BL": (10000, -2000),
             "GE": (3800, 1200),
-            "ZG": (7000, 8000),
+            "ZG": (1000, 1000),
             "SH": (-1000, 1000),
             "TI": (0, -5000),
-            "VD": (-6000, 1000),  # Example: move Bern slightly left
+            "VD": (-6000, 1000),
             "LU": (4000, 2000),
             "SO": (5000, 1000),
             "AI": (-1000, 0),
@@ -182,7 +197,7 @@ def create_canton_map_visualization(job_counts_input_path: Path, report_output_p
             "SG": (-8000, -3000),
             "OW": (-2000, 0),
             "NW": (2500, 2000),
-            "ZG": (-1000, -1000)
+
         }
         # --- 8. Add canton labels + job counts ---
         for _, row in centroids.iterrows():
@@ -235,7 +250,8 @@ if __name__ == "__main__":
     PROJECT_ROOT_TEST = Path(__file__).resolve().parent.parent.parent
     DATA_VIS_DIR_TEST = PROJECT_ROOT_TEST / "data" / "visualization"
     REPORT_DIR_TEST = PROJECT_ROOT_TEST / "report" / "figures"
-    TEST_INPUT_PATH = DATA_VIS_DIR_TEST / "jobs_ch_location_counts_1.csv"
+    # TEST_INPUT_PATH = DATA_VIS_DIR_TEST / "jobs_ch_location_counts_1.csv"
+    TEST_INPUT_PATH = PROJECT_ROOT_TEST / "data" / "analysis" / "jobs_ch_location_counts.csv"
     TEST_job_per_canton_output_path = DATA_VIS_DIR_TEST / "Job_per_canton.csv"
     TEST_OUTPUT_PATH = REPORT_DIR_TEST / "jobs_maps_switzerland.png"
 
